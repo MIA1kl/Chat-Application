7d68136ca467451fa8cb7f0b3f8bbfef2ba675b3
MIA1kl <mia01kln@gmail.com>
2021-06-30 02:47:01 +0600

    first commit

---
+++ app/src/main/java/com/example/android/chatapplication/ProfileSet.java
@@ -0,0 +1,14 @@
+package com.example.android.chatapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileSet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_set);
+    }
+}
